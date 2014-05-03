8ae1b4f02dd2a9c23754bb671929b64d73749ffa
Azri Jamil <azri@nematix.com>
2014-05-03 16:32:14 +0800

    Moving app config file to core package

---
+++ src/config/app.php
@@ -0,0 +1,20 @@
+<?php return array(
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Atlantis Config
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    */
+    'config' => array(
+        'enable' => true,
+        'setting_path' => public_path() . 'settings'
+    ),
+
+    'copyright' => '<a href="http://atlantis.nematix.com">Nematix Corporation</a>',
+
+);
