53846a9b45f19e399af16154e2b7294518422afd
Azri Jamil <azri@nematix.com>
2014-05-03 17:13:18 +0800

    Atlantis Core service provider

--- src/Atlantis/Core/CoreServiceProvider.php
+++ src/Atlantis/Core/CoreServiceProvider.php
@@ -1,6 +1,11 @@
 <?php namespace Atlantis\Core;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
+use Atlantis\Core\Client\Javascript;
+use Atlantis\Core\View;
+use Atlantis\Core\Config;
+
 
 class CoreServiceProvider extends ServiceProvider {
 
@@ -12,24 +17,56 @@ class CoreServiceProvider extends ServiceProvider {
 	protected $defer = false;
 
 	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('atlantis/core');
-	}
-
-	/**
 	 * Register the service provider.
 	 *
 	 * @return void
 	 */
 	public function register()
 	{
-		//
+        $this->app->bind('atlantis.client.javascript',function($app){
+            #i: Get configs
+            $view = $app['config']->get('core::client.javascript.bind');
+            $namespace = $app['config']->get('core::client.javascript.namespace');
+
+            #i: Get view binder
+            $binder = new View\Binder($app['events'],$view);
+
+            #i: Return provider instance
+            return new Javascript\Provider($binder,$namespace);
+        });
+
+        $this->app->bind('atlantis.config.fileloader', function($app){
+            #i: Get atlantis config file loader
+            $file_loader = new Config\FileLoader($app['files'], $app['path'].'/config');
+
+            #i: Replace default file loader
+            $app['config']->setLoader($file_loader);
+        });
 	}
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('atlantis/core');
+
+        #i: Automatic Alias loader
+        AliasLoader::getInstance()->alias(
+            'Javascript',
+            'Atlantis\Core\Client\Facades\Javascript'
+        );
+
+        #i: Activate Atlantis Config Fileloader
+        if( $this->app['config']->get('core::app.config.enable',true) ){
+            $this->app['atlantis.config.fileloader'];
+        }
+
+        #e: Event trigger
+        $this->app['events']->fire('atlantis.core.ready');
+    }
 
 	/**
 	 * Get the services provided by the provider.
@@ -38,7 +75,7 @@ class CoreServiceProvider extends ServiceProvider {
 	 */
 	public function provides()
 	{
-		return array();
+		return array('atlantis.client.javascript','atlantis.config.fileloader');
 	}
 
 }
