bb7ca11fdb85a478701da1053325396f328af826
Azri Jamil <azri@nematix.com>
2014-05-26 17:20:13 +0800

    Smart error partial view

--- src/views/partials/error.blade.php
+++ src/views/partials/error.blade.php
@@ -1,8 +1,15 @@
-<div class="padded">
-    @foreach ($errors->all() as $message )
-    <div class="alert alert-error">
-        <button class="close" data-dismiss="alert" type="button">×</button>
-        {{ $message }}
-    </div>
-    @endforeach
+@if( isset($_status) )
+<div class="alert alert-{{ $_status['type'] }}">
+    <button class="close" data-dismiss="alert" type="button">×</button>
+    {{ $_status['message'] }}
 </div>
+@else
+    @if($errors)
+        @foreach ($errors->all() as $message )
+        <div class="alert alert-error">
+            <button class="close" data-dismiss="alert" type="button">×</button>
+            {{ $message }}
+        </div>
+        @endforeach
+    @endif
+@endif
