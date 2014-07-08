fcfe2487f100fb73cafe81128da2372cf3f9f311
Azri Jamil <azri@nematix.com>
2014-07-08 14:12:45 +0800

    Minify base on environment

--- src/Atlantis/Asset/Collection/Javascript.php
+++ src/Atlantis/Asset/Collection/Javascript.php
@@ -7,8 +7,8 @@ use Assetic\Filter\JSMinFilter;
 class Javascript extends AssetCollection{
 
     public function __construct(array $assets=[], array $filters=[], $base_path=''){
-
-        if(!app('config')->get('app.debug'))  $filters[] = new JSMinFilter();
+        #i: Minified on production environment only
+        if( in_array(app()->environment(), app('config')->get('core::asset.cache.environment')) )  $filters[] = new JSMinFilter();
 
         parent::__construct($assets,$filters,$base_path);
     }
--- src/Atlantis/Asset/Collection/Stylesheet.php
+++ src/Atlantis/Asset/Collection/Stylesheet.php
@@ -10,21 +10,23 @@ use Atlantis\Asset\Assetic\Filter\UriPrependFilter;
 class Stylesheet extends AssetCollection{
 
     public function __construct(array $assets=[], array $filters=[], $base_path=''){
-        #i:::: Global filters
-        $filters = [
-            new UriRewriteFilter(),
-            new UriPrependFilter()
-        ];
-
-        if(!app('config')->get('app.debug'))  $filters[] = new CssMinFilter();
+        #i: Minified on production environment only
+        if( in_array(app()->environment(), app('config')->get('core::asset.cache.environment')) ) $filters[] = new CssMinFilter();
 
         parent::__construct($assets,$filters,$base_path);
     }
 
 
     protected function filterComposer($asset){
+        $file_ext = $this->files->extension($asset->getSourcePath());
+
+        #i: Uri filter
+        $asset->ensureFilter(new UriRewriteFilter());
+        $asset->ensureFilter(new UriPrependFilter('/ependahuluan'));
+
         #i: Apply filter based on extension
-        if( str_contains($asset->getSourcePath(),'less') ){
+        if( $file_ext == 'less' ){
+            $asset->setTargetPath( str_replace($file_ext,'css',$asset->getSourcePath()) );
             $asset->ensureFilter(new LessphpFilter());
         }
 
