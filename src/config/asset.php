6bcb779d50bded246579380c7a091ec19c5c5a9d
Azri Jamil <azri@nematix.com>
2014-07-08 14:13:45 +0800

    Refactor AAsset config file

---
+++ src/config/asset.php
@@ -0,0 +1,38 @@
+<?php return [
+
+    'build_path'    => public_path() . '/builds/assets',
+
+    'cache'         => [
+        'environment'   => ['local','production']
+    ],
+
+    'mimes'         => [
+        'stylesheet'    => ['css','less','scss'],
+        'javascript'    => ['js','coffee']
+    ],
+
+    'register' => array(
+        'prefixes' => array(
+            'component'  => public_path() . '/components/'
+        ),
+        'attributes' => array(
+            'appbase'   => app('config')->get('app.url')
+        )
+    ),
+
+    'assets' => array(
+        'default' => array(
+            'storage'   => 'file',
+            'path'      => public_path() . '/assets'
+        ),
+
+        'stylesheet' => array(
+            'component!bootstrap/css/bootstrap.css',
+            'component!bootstrap/css/bootstrap-theme.css'
+        ),
+        'javascript' => array(
+            'component!require.js',
+        )
+    )
+
+];
